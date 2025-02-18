0c4490e836d5b0b5f25a0822572408ca82132f4b
Evgen707 <mafit0705@gmail.com>
2025-02-18 20:14:45 +0300

    Создать веб-приложение1.2

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -1,23 +1,13 @@
-﻿using Microsoft.Analytics.Interfaces;
-using Microsoft.Analytics.Interfaces.Streaming;
-using Microsoft.Analytics.Types.Sql;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-
-namespace Store.Memory
+﻿namespace Store.Memory
 {
-    public class BookRepository : IBookRepository
+    public class BookRepository
     {
-
         private readonly Book[] books = new[]
         {
             new Book(1, "Art of programming"),
             new Book(2, "Refactoring"),
             new Book(3, "Programming Language"),
-        }
+        };
 
        public Book[] GetAllByTitle(string titlePart)
         {
